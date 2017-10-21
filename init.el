1a7624cb4fd98fd818bfdf91e5b997a4f958d1f7
allen7wang <allen7wang@gmail.com>
2017-10-21 12:52:15 +0800

    fix keybindings

--- init.el
+++ init.el
@@ -7,88 +7,22 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
-(require 'init-packages)
-
-(setq ring-bell-function 'ignore)
-
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-
-(global-auto-revert-mode t)
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(electric-indent-mode t)
-(setq inhibit-splash-screen t)
-
-(global-linum-mode t)
-
-(abbrev-mode t)
-(define-abbrev-table 'global-abbrev-table '(
-					    ;; signature
-					    ("8wc" "allen7wang")
-					    ;; emacs regex
-					    ))
-
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
-(global-set-key (kbd "<f2>") 'open-my-init-file)
+;; add more personal func
+;; new init-func.el
+
+;; (require 'init-func.el)
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-keybindings)
+(require 'init-org)
 
 
-(setq-default cursor-type 'bar )
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-  (require 'org)
-  (setq org-src-fontify-natively t)
-
-(require 'recentf)
-(recentf-mode  1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-(delete-selection-mode t)
-
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
-
-(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
-
-(global-hl-line-mode t)
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory)) 
 
 
-
-(global-set-key (kbd "C-h C-f") 'find-function)
-(global-set-key (kbd "C-h C-v") 'find-variable)
-(global-set-key (kbd "C-h C-k") 'find-function-on-key)
-
-(setq org-agenda-files '("~/Documents/Org/Agenda"))
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 1)
- '(custom-safe-themes
-   (quote
-    ("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
- '(package-selected-packages (quote (monokai-theme company hungry-delete)))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js2-external-variable ((t (:foreground "dark gray")))))
+(load-file custom-file)
